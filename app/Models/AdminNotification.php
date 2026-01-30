1960d9293511055f28de72a48ee0ea066723f1e7
rizwanyng <rizwan@yngmedia.com>
2026-01-31 00:15:59 +0530

    Update AdminNotification model

--- app/Models/AdminNotification.php
+++ app/Models/AdminNotification.php
@@ -9,10 +9,38 @@ class AdminNotification extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'title', 'body', 'is_read'];
+    protected $fillable = ['user_id', 'title', 'body', 'is_read', 'type', 'metadata'];
+
+    protected $casts = [
+        'is_read' => 'boolean',
+        'metadata' => 'array',
+    ];
+
+    public function scopeUnread($query)
+    {
+        return $query->where('is_read', false);
+    }
+
+    public function markAsRead()
+    {
+        $this->update(['is_read' => true]);
+    }
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function broadcast($title, $body, $type = 'info')
+    {
+        $users = \App\Models\User::all();
+        foreach ($users as $user) {
+            self::create([
+                'user_id' => $user->id,
+                'title' => $title,
+                'body' => $body,
+                'type' => $type,
+            ]);
+        }
     }
 }
